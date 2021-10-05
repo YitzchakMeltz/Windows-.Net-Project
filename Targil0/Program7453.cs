5df0a7db1e531b36753b0e90369040c4c2d375f9
Eliezer Jacobs <eliezerjacobs118@gmail.com>
2021-10-05 12:26:01 +0300

    step 6

---
+++ Targil0/Program7453.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome7453();
+            Welcome3510();
+            Console.ReadKey(true);
+        }
+
+        static partial void Welcome3510();
+        private static void Welcome7453()
+        {
+            Console.Write("Enter your name: ");
+            Console.WriteLine("{0}, welcome to my first console application", Console.ReadLine());
+            Console.Write("Press any key to continue . . . ");
+        }
+    }
+}
