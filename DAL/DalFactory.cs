354db27d4f223499d4e58e1495fd4fbbb95661ba
YitzchakMeltz <75029003+YitzchakMeltz@users.noreply.github.com>
2021-12-19 17:13:54 +0200

    create GetDal static function

---
+++ DAL/DalFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DalApi;
+using DO;
+
+namespace DalObject
+{
+    public static class DalFactory
+    {
+        public static DalApi.IDal GetDal(string objectType)
+        {
+            DalApi.IDal returnValue = new DalObject();
+
+            if (objectType == "DalObject")
+            {
+                return returnValue;
+            }
+
+            else if(objectType == "DalXml")
+            {
+                //returnValue = new DalXml();
+            }
+
+            else
+            {
+                throw new StringNotValid($"{objectType} is an invalid string. Must be 'DalObject' or 'DalXml'");
+            }
+
+            return returnValue;
+        }
+    }
+}
--- DAL/Exceptions.cs
+++ DAL/Exceptions.cs
@@ -13,4 +13,11 @@
             public ObjectNotFound(string message) : base(message) { }
             public ObjectNotFound(string message, System.Exception inner) : base(message, inner) { }
         }
+
+        public class StringNotValid : System.Exception
+        {
+            public StringNotValid() { }
+            public StringNotValid(string message) : base(message) { }
+            public StringNotValid(string message, System.Exception inner) : base(message, inner) { }
+        }
     }
