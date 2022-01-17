d044a52006a70f3488989416a516726c5700d31f
Eliezer Jacobs <eliezerjacobs118@gmail.com>
2022-01-17 22:02:54 +0200

    xml start

---
+++ DalXml/DalXML.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Dal
+{
+    internal partial class DalXml : DalApi.IDal
+    { 
+        private static readonly Lazy<DalXml> lazy = new Lazy<DalXml>(() => new DalXml());
+        public static DalXml Instance
+        {
+            get
+            {
+                return lazy.Value;
+            }
+        }
+    }
+}
